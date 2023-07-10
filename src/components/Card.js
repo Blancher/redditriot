ca51d4bf9a752268d52da08f8ebc34c3ee386f81
Ben Saacks <108644584+Blancher@users.noreply.github.com>
2023-07-10 09:03:54 -0700

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,14 +10,14 @@ export default function Card(props) {
         navigate(`/${props.subreddit}/${props.id}`);
         dispatch(postActions.setPost(props.permalink));
     };
-    
+
     return (
         <div className={`postcard ${props.className ? props.className : ''} `} onClick={handleClick}>
             <p className='user'>Posted by <i>{props.author}</i></p>
             {props.className === 'comment' ? <h4 className='commentFlow'>{props.title || props.body}</h4> : <h4 className={(props.thumbnail && props.thumbnail !== 'self') ? 'overflow' : ''}>{props.title || props.body}</h4>}
             <h5 className='cardsub'><b>r/{props.subreddit} ✓ {props.ups}</b></h5>
             {props.className !== 'comment' && <h5 className='numcoms'>💬 {props.num_comments} Comment{props.num_comments === 1 ? '' : 's'}</h5>}
-            {(props.thumbnail && props.thumbnail !== 'self') && <img className='media' src={props.thumbnail}/>}
+            {(props.thumbnail && props.thumbnail !== 'self' && props.thumbnail !== 'spoiler') && <img className='media' src={props.thumbnail}/>}
         </div>
     );
 }
